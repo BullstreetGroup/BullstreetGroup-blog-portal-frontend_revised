caab8ea1a7d859e5a50c0517a82eea5378ab1cc9
Anurag-Erpca <142516580+Anurag-Erpca@users.noreply.github.com>
2024-09-07 17:40:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    chunkSizeWarningLimit: 2000,
-  },
-})
+    outDir: 'dist' // This is the default, so you might not need this line unless you’ve customized it
+  }
+});
+
+
